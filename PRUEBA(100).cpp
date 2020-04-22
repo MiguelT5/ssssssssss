3df1d6539ee10b3d9e1acae09788f6a4a747d1e9
sofia p <sofiiapin26@gmail.com>
2020-04-21 20:08:59 -0500

    segundo intento

---
+++ PRUEBA(100).cpp
@@ -0,0 +1,16 @@
+  #include<iostream>
+  using namespace std;
+  
+  int main(){
+  	float N1,n2;
+  	
+  	cout<<"ingresa un numero para dividirlo entre 100"<<endl;
+  	cin>>N1;
+  	
+  	n2=(N1/100);
+  	
+  	cout<<"\nel resultado de la division es: " <<n2;
+  	
+  	return 0;
+  	
+  }
