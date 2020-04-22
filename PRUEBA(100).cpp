39f8a968552531af62da279df2fe60ac37eea83a
MiguelT5 <63890818+MiguelT5@users.noreply.github.com>
2020-04-21 21:20:02 -0500

    agrego mas cosas
    porque si, nose que mas agregar equis de.

--- PRUEBA(100).cpp
+++ PRUEBA(100).cpp
@@ -2,15 +2,17 @@
   using namespace std;
   
   int main(){
-  	float N1,n2;
+  	float N1,n2,N3,n4;
   	
   	cout<<"ingresa un numero para dividirlo entre 100"<<endl;
   	cin>>N1;
   	
-  	n2=(N1/100);
-  	
-  	cout<<"\nel resultado de la division es: " <<n2;
-  	
+  	n2=(N1/50);
+  	N3=(n2+N1)
+    n4=-(N3*1000)
+  	cout<<"\nel resultado de la division es: " <<n2<<endl;
+  	cout<<"la suma es: "<<N3<<endl;
+    cout<<"por mil: "<<n4<<endl;
   	return 0;
   	
   }
